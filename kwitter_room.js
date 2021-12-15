37c1314c0902c9d0ad7cef3c717aa79386da0318
suriburi123 <83498701+suriburi123@users.noreply.github.com>
2021-12-15 17:24:35 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,16 @@
+
+
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
+
